68d2111a0b9621fe55b008afc82db920238cbd12
serghi.p <83840254+serghip@users.noreply.github.com>
2022-06-08 09:26:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
 
 // middlewares
-app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
